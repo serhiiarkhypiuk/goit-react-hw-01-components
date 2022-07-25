e3485fb85b95e1d134fa358de46a9ee3adbd4bed
serhiiarkhypiuk <airborn.gnus@gmail.com>
2022-07-25 13:50:49 +0300

    styles added

--- src/index.js
+++ src/index.js
@@ -3,19 +3,19 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Profile from './components/Profile/Profile';
-import user from './user.json';
+import user from './data/user.json';
 
 import Statistics from './components/Statistics/Statistics';
-import data from './data.json';
+import data from './data/data.json';
 
 import FriendList from 'components/FriendList/FriendList';
-import friends from './friends.json';
+import friends from './data/friends.json';
 
 import TransactionHistoryTable from './components/TransactionHistoryTable/TransactionHistoryTable';
-import transactions from './transactions.json';
+import transactions from './data/transactions.json';
 
 import Button from 'components/Button/Button';
-import buttons from './buttons.json';
+import buttons from './data/buttons.json';
 import styles from './components/Button/Button.module.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
